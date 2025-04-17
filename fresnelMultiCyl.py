1e67a9d450c7c20529662c3d1e4988c946062edd
YossiLev <yossi.lev.home@gmail.com>
2025-04-17 20:40:24 +0300

    normalization and 0.5 push

--- fresnelMultiCyl.py
+++ fresnelMultiCyl.py
@@ -52,25 +52,44 @@ def cylindrical_abcd_propagation_batch(U1_batch, r, wavelength, abcd):
 
 # Parameters
 wavelength = 633e-9
-z = 8.59
+z = 1
 A, B, C, D = 1, z, 0, 1
 
 # Radial grid
-N_r = 300
-r = np.linspace(0, 5e-3, N_r)
+N_r = 128
+max_val = 5e-3
+r = np.linspace(0, max_val, N_r)
+sh = r[1] * 0.5
+r = r + sh
 
 # Example: 3 Gaussian beams with different waists
-w0_list = [1e-3, 0.5e-3, 1.5e-3]
-U1_batch = np.array([np.exp(-r**2 / w0**2) for w0 in w0_list])
+w0_list = np.linspace(0.2e-3, 1e-3, 10)
+print("Waists (m):", w0_list)
+rayligh = np.pi * np.square(w0_list) / wavelength
+print("Rayleigh lengths (m):", rayligh)
+target_waists = w0_list * np.sqrt(1 + np.square((z / rayligh)))
+print("Target waists (m):", target_waists)
+target_top_values = w0_list / target_waists
+print("Target top values:", target_top_values)
+
+#U1_batch = np.array([np.exp(-r**2 / w0**2) for w0 in w0_list])
+U1_batch = np.exp(-np.square(r.reshape(-1, 1) / w0_list.reshape(1, -1))).T
 
 # Propagate all
-U2_batch = cylindrical_abcd_propagation_batch(U1_batch, r, wavelength, (A, B, C, D)) * 12.6
+U2_batch = (cylindrical_abcd_propagation_batch(U1_batch, r, wavelength, (A, B, C, D)) * 2 * np.pi) #.T / target_top_values).T
 
 # Plotting
 plt.figure(figsize=(8, 4))
 for i, w0 in enumerate(w0_list):
-    plt.plot(r * 1e3, np.abs(U1_batch[i]), label=f"b0 = {w0*1e3:.1f} mm")
-    plt.plot(r * 1e3, np.abs(U2_batch[i]), label=f"w0 = {w0*1e3:.1f} mm")
+    plt.plot(r * 1e3, np.abs(U1_batch[i]), label=f"before = {w0*1e3:.1f} mm")
+for i, w0 in enumerate(w0_list):
+    plt.plot(r * 1e3, np.abs(U2_batch[i]) + 1, label=f"after = {w0*1e3:.1f} mm")
+top_values = np.abs(U2_batch[:, 0])
+print("Top values:", top_values)
+miss_by = target_top_values / top_values # / (2 * np.pi)
+print("Miss by:", miss_by)
+dev = (miss_by - 1) * 1000
+print("Deviation (mm):", dev)
 plt.xlabel("r (mm)")
 plt.ylabel("Amplitude |U2(r)|")
 plt.grid(True)
